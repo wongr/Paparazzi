ac2d6645ff61d9bcd47498c22534d81dadfd1e1f
Wang Ran <ran@Wang-RanmatoMacBook-Air.local>
2011-05-15 22:25:48 +0800

    Initial Commit

---
+++ Paparazzi/PaparazziAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  PaparazziAppDelegate.h
+//  Paparazzi
+//
+//  Created by Wang Ran on 11-5-15.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PaparazziAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ Paparazzi/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Paparazzi
+//
+//  Created by Wang Ran on 11-5-15.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
